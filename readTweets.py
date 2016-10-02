48c91d7dc2094727bc9eb97a44292165b8902516
frammnm@gmail.com <frammnm@gmail.com>
2016-10-02 15:01:25 -0430

    Cambios para arreglar la codificacion de los strings

--- readTweets.py
+++ readTweets.py
@@ -1,8 +1,34 @@
+import re
 import sys
+from nltk.tokenize import word_tokenize
+import codecs
 
+
+ 
+emoticons_str = r"""
+    (?:
+        [:=;] # Eyes
+        [oO\-]? # Nose (optional)
+        [D\)\]\(\]/\\OpP] # Mouth
+    )"""
+ 
+regex_str = [
+    emoticons_str,
+    r'<[^>]+>', # HTML tags
+    r'(?:@[\w_]+)', # @-mentions
+    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
+    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
+ 
+    r'(?:(?:\d+,?)+(?:\.?\d+)?)', # numbers
+    r"(?:\w[a-z'\-_]+\w)", # words with - and '
+    r'(?:[\w_]+)', # other words
+    r'(?:\S)' # anything else
+]
+    
 def read_tweets(name):
   try:
-    f = open(name, 'r') 
+    # f = open(name, 'r') 
+    f = codecs.open(name, encoding='utf-8')
     data = f.readlines()
     account = ""
     tweets = []
@@ -14,16 +40,44 @@ def read_tweets(name):
         info = line.split("][")
         info.append(account)
         info[0] = info[0][1:] # Removing "["
-        info[1] = info[1].decode("utf-8", "ignore")
+        info[1] = info[1].decode("utf-8")
         info[2] = info[2][:-1] # Removing "\n"
         info[2] = info[2][:-1] # Removing "]"
-        tweets.append(info)
+        elem = {
+          'owner':account,
+          'date':info[0],
+          'text':info[1],
+          'retweets':info[2]
+        }
+        tweets.append(elem)
     f.close()
     return tweets
   except IOError as e:
     print "I/O error({0}): {1}".format(e.errno, e.strerror)
     sys.exit(1)
+ 
+def tokenize(s):
+    return tokens_re.findall(s)
+ 
+def preprocess(s, lowercase=False):
+    tokens = tokenize(s)
+    if lowercase:
+        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+    return tokens
+
 
 if __name__ == "__main__":
-  for elem in read_tweets("prueba.txt"):
-    print elem[1].encode('utf-8')
+  reload(sys)
+  sys.setdefaultencoding('utf8')
+  # for elem in read_tweets("prueba.txt"):
+  #   print elem[1].encode('utf-8')
+  tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE | re.UNICODE )
+  emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE | re.UNICODE)
+  
+  tweets = read_tweets("tweets.txt")
+  for tweet in tweets:
+    tweet['tokens'] = preprocess(tweet['text'])
+    # print(tweet['tokens'])
+    for token in tweet['tokens']:
+      print("token "+ token.encode('utf-8'))
+
